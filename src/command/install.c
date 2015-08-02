58a75b355300a258a6fb2c9f141bf2b83518fa56
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-02 17:16:33 +0900

    wip

--- src/command/install.c
+++ src/command/install.c
@@ -1,5 +1,10 @@
 #include "command.h"
 
 int cmd_install(int argc, const char** argv) {
+  const char* text = "{\"hello\": \"world\"}";
+  json_error_t error;
+  json_t* root = json_loads(text, 0, &error);
   printf("hello, world\n");
+  printf("hello: %s\n", json_string_value(json_object_get(root, "hello")));
+  json_decref(root);
 }
