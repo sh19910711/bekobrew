375659a2a9d7e4b78c459a5f331879dfc920463d
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-02 17:55:10 +0900

    wip

--- src/command/install.c
+++ src/command/install.c
@@ -99,8 +99,20 @@ int cmd_install(int argc, const char** argv) {
   char* text = request(URL);
   json_error_t error;
   json_t* root = json_loads(text, 0, &error);
+  json_t* item;
+  json_t* sha;
+  json_t* commit;
+  json_t* message;
   free(text);
 
+  int i;
+  for (i = 0; i < json_array_size(root); i++) {
+    item = json_array_get(root, i);
+    sha = json_object_get(item, "sha");
+    commit = json_object_get(item, "commit");
+    message = json_object_get(commit, "message");
+    printf("%s: %s\n", json_string_value(sha), json_string_value(message));
+  }
 
   json_decref(root);
 }
