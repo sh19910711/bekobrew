b5d45f8fa0b5e9b8e6c6539a07c925f04a0e81d5
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-02 16:19:44 +0900

    wip

--- src/command.h
+++ src/command.h
@@ -2,6 +2,9 @@
 #define COMMAND_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <jansson.h>
 
 #include "version.h"
 #include "inline_utils.h"
--- src/command/install.c
+++ src/command/install.c
@@ -1,5 +1,4 @@
 #include "command.h"
-#include <jansson.h>
 
 int cmd_install(int argc, const char** argv) {
   printf("hello, world\n");
