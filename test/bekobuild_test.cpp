9459dca626b51a874ceb2b59722ae7e74a53545c
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-21 02:21:57 +0000

    wip

---
+++ test/bekobuild_test.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+  #include "bekobuild.h"
+}
+
+TEST(BEKOBUILD, SimplePackage) {
+  const char* input =
+    "name: pkg\n"
+    "build:\n"
+    "  - ./configure\n"
+    "  - make\n"
+    "package:\n"
+    "  - make install";
+  FILE* fp = fmemopen((void*) input, strlen(input), "r");
+  struct bekobuild_t* bekobuild = open_bekobuild(fp);
+  close_bekobuild(bekobuild);
+  fclose(fp);
+}
+
+TEST(BEKOBUILD, LoadFromFile) {
+  FILE* fp = fopen("./test/bekobuild_test/BEKOBUILD", "r");
+  struct bekobuild_t* bekobuild = open_bekobuild(fp);
+  close_bekobuild(bekobuild);
+  fclose(fp);
+}
