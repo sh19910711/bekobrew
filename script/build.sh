f1f4ec8cdedf2f3921285ec713302e6934f3cc33
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-21 14:51:48 +0900

    wip

--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
+uname_grep() {
+  uname -a | grep $@ 1>/dev/null 2>/dev/null
+}
+
+call_make() {
+  if uname_grep SunOS; then
+    make
+  else
+    make -j4
+  fi
+}
+
 mkdir -p build && cd build
-cmake $@ .. && make -j4
+cmake $@ .. && call_make
