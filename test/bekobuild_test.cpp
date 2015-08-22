44af257d3d62d0566fe1356051ee1c56d4caf436
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-22 13:40:20 +0000

    wip: bekobuild_test

--- test/bekobuild_test.cpp
+++ test/bekobuild_test.cpp
@@ -9,11 +9,11 @@ TEST(BEKOBUILD, SimplePackage) {
   struct bekobuild_t* bekobuild = bekobuild_open(fp);
 
   EXPECT_STREQ("fake-package", bekobuild->name);
-  EXPECT_EQ(2, bekobuild->build->n);
-  EXPECT_STREQ("./configure", bekobuild->build->data[0]);
-  EXPECT_STREQ("make", bekobuild->build->data[1]);
-  EXPECT_EQ(1, bekobuild->package->n);
-  EXPECT_STREQ("make install", bekobuild->package->data[0]);
+  EXPECT_EQ(2, bekobuild->build->size);
+  EXPECT_STREQ("./configure", node_to_string(vector_at(bekobuild->build, 0)));
+  EXPECT_STREQ("make", node_to_string(vector_at(bekobuild->build, 1)));
+  EXPECT_EQ(1, bekobuild->package->size);
+  EXPECT_STREQ("make install", node_to_string(vector_at(bekobuild->package, 0)));
 
   bekobuild_close(bekobuild);
   fclose(fp);
