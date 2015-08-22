32a061450999d6fa8a85533280f5f1b736c3651c
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-22 13:40:42 +0000

    wip: vector_test

---
+++ test/vector_test.cpp
@@ -0,0 +1,46 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+  #include "vector.h"
+}
+
+const char *fake_items[] = { "item-0", "item-1", "item-2" };
+
+class VectorTest : public ::testing::Test {
+protected:
+  struct vector_t *v;
+
+  virtual void SetUp() {
+    v = vector_new();
+  }
+
+  virtual void TearDown() {
+    vector_free(v);
+  }
+
+  void push_fake_nodes() {
+    for (int i = 0; i < 3; ++i) {
+      struct node_t *node = node_new();
+      node_set_value(node, fake_items[i]);
+      vector_push(v, node);
+    }
+  }
+};
+
+TEST_F(VectorTest, AttrsBeforePushNodes) {
+  EXPECT_EQ(0, v->size);
+  EXPECT_EQ(1, v->capacity);
+}
+
+TEST_F(VectorTest, AttrsAfterPushNodes) {
+  push_fake_nodes();
+  EXPECT_EQ(3, v->size);
+  EXPECT_EQ(4, v->capacity);
+}
+
+TEST_F(VectorTest, CheckNodes) {
+  push_fake_nodes();
+  EXPECT_STREQ("item-0", node_to_string(vector_at(v, 0)));
+  EXPECT_STREQ("item-1", node_to_string(vector_at(v, 1)));
+  EXPECT_STREQ("item-2", node_to_string(vector_at(v, 2)));
+}
