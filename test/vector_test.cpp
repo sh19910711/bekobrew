540aac3265dfa87bceb86fd4cd1919384cdb4992
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-22 16:40:48 +0000

    wip: vector_test

--- test/vector_test.cpp
+++ test/vector_test.cpp
@@ -20,27 +20,25 @@ protected:
 
   void push_fake_nodes() {
     for (int i = 0; i < 3; ++i) {
-      struct node_t *node = node_new();
-      node_set_value(node, fake_items[i]);
-      vector_push(v, node);
+      vector_push(v, node_new(fake_items[i]));
     }
   }
 };
 
 TEST_F(VectorTest, AttrsBeforePushNodes) {
-  EXPECT_EQ(0, v->size);
-  EXPECT_EQ(1, v->capacity);
+  ASSERT_EQ(0, v->size);
+  ASSERT_EQ(1, v->capacity);
 }
 
 TEST_F(VectorTest, AttrsAfterPushNodes) {
   push_fake_nodes();
-  EXPECT_EQ(3, v->size);
-  EXPECT_EQ(4, v->capacity);
+  ASSERT_EQ(3, v->size);
+  ASSERT_EQ(4, v->capacity);
 }
 
 TEST_F(VectorTest, CheckNodes) {
   push_fake_nodes();
-  EXPECT_STREQ("item-0", node_to_string(vector_at(v, 0)));
-  EXPECT_STREQ("item-1", node_to_string(vector_at(v, 1)));
-  EXPECT_STREQ("item-2", node_to_string(vector_at(v, 2)));
+  ASSERT_STREQ("item-0", node_to_string(vector_at(v, 0)));
+  ASSERT_STREQ("item-1", node_to_string(vector_at(v, 1)));
+  ASSERT_STREQ("item-2", node_to_string(vector_at(v, 2)));
 }
