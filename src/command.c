fcb987cb502417c5a7c8a22b1413dd8af55b3772
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-22 04:35:36 +0000

    wip: command

--- src/command.c
+++ src/command.c
@@ -1,6 +1,6 @@
 #include "command.h"
 
-static struct command commands[] = {
+static struct command sub_commands[] = {
   { "version", sub_command_version },
   { "help", sub_command_help },
   { "install", sub_command_install },
@@ -8,8 +8,8 @@ static struct command commands[] = {
 
 struct command *command_find(const char *cmd_name) {
   int i;
-  for (i = 0; i < ARRAY_SIZE(commands); i++) {
-    struct command *p = commands + i;
+  for (i = 0; i < ARRAY_SIZE(sub_commands); i++) {
+    struct command *p = sub_commands + i;
     if (!strcmp(cmd_name, p->cmd)) {
       return p;
     }
