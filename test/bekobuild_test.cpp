b0cf55dc2377a44cceedce4b3e07c6e0ad8ee676
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-22 04:57:25 +0000

    wip: bekobuild_test

--- test/bekobuild_test.cpp
+++ test/bekobuild_test.cpp
@@ -7,12 +7,12 @@ extern "C" {
 TEST(BEKOBUILD, LoadFromFile) {
   FILE* fp = fopen("./test/bekobuild_test/BEKOBUILD", "r");
   struct bekobuild_t* bekobuild = bekobuild_open(fp);
-  
+
   EXPECT_STREQ(bekobuild_get_name(bekobuild), "fake-package");
   EXPECT_STREQ(bekobuild_get_build(bekobuild)[0], "./configure");
   EXPECT_STREQ(bekobuild_get_build(bekobuild)[1], "make");
   EXPECT_STREQ(bekobuild_get_package(bekobuild)[0], "make install");
-  
+
   bekobuild_close(bekobuild);
   fclose(fp);
 }
