df81fbc1ef24bbc253852688e9bd7940379dd3d6
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-24 03:10:38 +0900

    wip: string_map_test

--- test/string_map_test.cpp
+++ test/string_map_test.cpp
@@ -6,32 +6,32 @@ extern "C" {
 
 class MapTest : public ::testing::Test {
 protected:
-  struct map_t *self;
+  struct string_map_t *self;
 
   virtual void SetUp() {
-    self = map_new();
+    self = string_map_new();
   }
 
   virtual void TearDown() {
-    map_free(self);
+    string_map_free(self);
   }
 };
 
 TEST_F(MapTest, SetAndGet) {
-  ASSERT_TRUE(map_set(self, "hello", "world"));
-  ASSERT_TRUE(map_set(self, "world", "hello"));
-  ASSERT_STREQ("world", map_get_as_string(self, "hello"));
-  ASSERT_STREQ("hello", map_get_as_string(self, "world"));
+  ASSERT_TRUE(string_map_set(self, "hello", "world"));
+  ASSERT_TRUE(string_map_set(self, "world", "hello"));
+  ASSERT_STREQ("world", string_map_get_as_string(self, "hello"));
+  ASSERT_STREQ("hello", string_map_get_as_string(self, "world"));
 }
 
 TEST_F(MapTest, OverrideValue) {
-  ASSERT_TRUE(map_set(self, "hello", "world"));
-  ASSERT_TRUE(map_set(self, "world", "hello"));
-  ASSERT_TRUE(map_set(self, "hello", "world2"));
-  ASSERT_STREQ("world2", map_get_as_string(self, "hello"));
-  ASSERT_STREQ("hello", map_get_as_string(self, "world"));
+  ASSERT_TRUE(string_map_set(self, "hello", "world"));
+  ASSERT_TRUE(string_map_set(self, "world", "hello"));
+  ASSERT_TRUE(string_map_set(self, "hello", "world2"));
+  ASSERT_STREQ("world2", string_map_get_as_string(self, "hello"));
+  ASSERT_STREQ("hello", string_map_get_as_string(self, "world"));
 }
 
 TEST_F(MapTest, NotFound) {
-  ASSERT_FALSE(map_get_as_string(self, "not-found"));
+  ASSERT_FALSE(string_map_get_as_string(self, "not-found"));
 }
