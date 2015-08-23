03ec2a4346a4114fc6d1e8a0edc5ce73269c058e
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-23 17:21:15 +0900

    wip: test_helper

--- test/test_helper.h
+++ test/test_helper.h
@@ -2,5 +2,15 @@
 #define TEST_HELPER
 
 #include <gtest/gtest.h>
+#include <gtest/internal/gtest-port.h>
+#include <sstream>
+
+static inline void test_capture() {
+  testing::internal::CaptureStdout();
+}
+
+static inline const char *test_output() {
+  return testing::internal::GetCapturedStdout().c_str();
+}
 
 #endif
