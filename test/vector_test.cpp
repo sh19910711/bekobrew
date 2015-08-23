d03a5f1812728f8a2229f6b2fcfaae5a3d14117e
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-23 11:40:48 +0900

    wip: vector_test

--- test/vector_test.cpp
+++ test/vector_test.cpp
@@ -1,29 +1,5 @@
-#include <gtest/gtest.h>
-
-extern "C" {
-  #include "vector.h"
-}
-
-const char *fake_items[] = { "item-0", "item-1", "item-2" };
-
-class VectorTest : public ::testing::Test {
-protected:
-  struct vector_t *v;
-
-  virtual void SetUp() {
-    v = vector_new();
-  }
-
-  virtual void TearDown() {
-    vector_free(v);
-  }
-
-  void push_fake_nodes() {
-    for (int i = 0; i < 3; ++i) {
-      vector_push(v, node_new(fake_items[i]));
-    }
-  }
-};
+#include "./test_helper.h"
+#include "./vector_test.h"
 
 TEST_F(VectorTest, AttrsBeforePushNodes) {
   ASSERT_EQ(0, v->size);
---
+++ test/vector_test.h
@@ -0,0 +1,29 @@
+#ifndef VECTOR_TEST
+#define VECTOR_TEST
+
+extern "C" {
+  #include "vector.h"
+}
+
+const char *fake_items[] = { "item-0", "item-1", "item-2" };
+
+class VectorTest : public ::testing::Test {
+protected:
+  struct vector_t *v;
+
+  virtual void SetUp() {
+    v = vector_new();
+  }
+
+  virtual void TearDown() {
+    vector_free(v);
+  }
+
+  void push_fake_nodes() {
+    for (int i = 0; i < 3; ++i) {
+      vector_push(v, node_new(fake_items[i]));
+    }
+  }
+};
+
+#endif
