6f83da51b0cae3434ffbd9ae5cf3a4560e10f125
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-23 02:31:41 +0000

    wip: bekobuild_test

--- test/bekobuild_test.cpp
+++ test/bekobuild_test.cpp
@@ -1,33 +1,5 @@
 #include "./test_helper.h"
-
-extern "C" {
-  #include "bekobuild.h"
-}
-
-class BekobuildTest : public ::testing::Test {
-protected:
-  FILE *fp;
-  struct bekobuild_t *bekobuild;
-
-  virtual void SetUp() {
-    fp = fopen(path(), "r");
-    bekobuild = bekobuild_open(fp);
-  }
-
-  virtual void TearDown() {
-    bekobuild_close(bekobuild);
-    fclose(fp);
-  }
-
-  virtual const char *path() = 0;
-};
-
-class BekobuildSimplePackage : public BekobuildTest {
-protected:
-  const char *path() {
-    return "./test/bekobuild_test/SimplePackage";
-  }
-};
+#include "./bekobrew_test.h"
 
 TEST_F(BekobuildSimplePackage, Name) {
   ASSERT_STREQ("fake-package", bekobuild->name);
