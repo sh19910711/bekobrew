8088cc929844b3065720be678c8dd2479f7a06c7
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-23 17:21:04 +0900

    wip: string_utils

---
+++ src/string_utils.h
@@ -0,0 +1,20 @@
+#ifndef STRING_UTILS_H
+#define STRING_UTILS_H
+
+static inline int string_start_with(const char *s, const char c) {
+  return s[0] == c;
+}
+
+static inline int string_skip_prefix(const char *s, const char *prefix,
+                              const char **out) {
+  do {
+    if(!*prefix) {
+      *out = s;
+      return 1;
+    }
+  } while(*s++ == *prefix++);
+
+  return 0;
+}
+
+#endif
