b5f0998ccf04aad0c5ad7ec29ec458b121a517d2
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-24 03:04:07 +0900

    wip: command

--- src/command.h
+++ src/command.h
@@ -4,17 +4,17 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "vector.h"
+#include "string_vector.h"
 #include "version.h"
 
 struct command {
   const char *cmd;
-  int (*call)(struct vector_t *);
+  int (*call)(struct string_vector_t *);
 };
 
-extern int sub_command_version(struct vector_t *);
-extern int sub_command_help(struct vector_t *);
-extern int sub_command_install(struct vector_t *);
+extern int sub_command_version(struct string_vector_t *);
+extern int sub_command_help(struct string_vector_t *);
+extern int sub_command_install(struct string_vector_t *);
 
 extern struct command *command_find(const char *cmd_name);
 
