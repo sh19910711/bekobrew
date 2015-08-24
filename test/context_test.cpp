508b2fcb1a5ac09f1a2fd1219ef43d70bd63928a
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-24 14:28:32 +0900

    wip: context_test

--- test/context_test.cpp
+++ test/context_test.cpp
@@ -4,7 +4,7 @@ extern "C" {
   #include "context.h"
 }
 
-class ExpandTest : public ::testing::Test {
+class ContextTest : public ::testing::Test {
 protected:
   struct context_t *context;
   struct string_map_t *map;
@@ -18,44 +18,44 @@ protected:
   }
 };
 
-class ExpandSingleVariable : public ExpandTest {
+class ContextSingleVariable : public ContextTest {
 protected:
   virtual void SetUp() {
-    ExpandTest::SetUp();
+    ContextTest::SetUp();
     string_map_set(context->map, "name", "value");
     context_calc_max_length(context);
   }
 };
 
-TEST_F(ExpandSingleVariable, NoVariables) {
+TEST_F(ContextSingleVariable, NoVariables) {
   char *expanded = context_expand_string(context, "hello, world.");
   EXPECT_STREQ("hello, world.", expanded);
   free(expanded);
 }
 
-TEST_F(ExpandSingleVariable, Name) {
+TEST_F(ContextSingleVariable, Name) {
   char *expanded = context_expand_string(context, "hello, $name.");
   EXPECT_STREQ("hello, value.", expanded);
   free(expanded);
 }
 
-TEST_F(ExpandSingleVariable, MissingName) {
+TEST_F(ContextSingleVariable, MissingName) {
   char *expanded = context_expand_string(context, "hello, $missing.");
   EXPECT_STREQ("hello, $missing.", expanded);
   free(expanded);
 }
 
-class ExpandMultipleVariables : public ExpandTest {
+class ContextMultipleVariables : public ContextTest {
 protected:
   virtual void SetUp() {
-    ExpandTest::SetUp();
+    ContextTest::SetUp();
     string_map_set(context->map, "name", "pkg");
     string_map_set(context->map, "version", "1.2.3");
     context_calc_max_length(context);
   }
 };
 
-TEST_F(ExpandMultipleVariables, ExpandPackageName) {
+TEST_F(ContextMultipleVariables, ContextPackageName) {
   char *expanded = context_expand_string(context, "$name-$version.tar.gz");
   EXPECT_STREQ("pkg-1.2.3.tar.gz", expanded);
   free(expanded);
