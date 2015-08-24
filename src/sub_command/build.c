540c678324a8fd2a4479a5575cedfd82183c46c2
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-25 02:03:18 +0900

    wip: sub_command

--- src/sub_command/build.c
+++ src/sub_command/build.c
@@ -6,7 +6,7 @@ int sub_command_build(struct string_vector_t *args) {
   FILE *fp = fopen("./BEKOBUILD", "r");
   struct bekobuild_t *src = bekobuild_new();
   bekobuild_open(src, fp);
-  struct bekobuild_t *bekobuild = bekobuild_eval(src);
+  struct bekobuild_t *bekobuild = bekobuild_expand(src);
   eval(bekobuild);
   bekobuild_free(src);
   bekobuild_free(bekobuild);
