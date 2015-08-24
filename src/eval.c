690035231b2aeade3470303e7f1485c875ac85cb
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-24 14:29:06 +0900

    wip: eval

---
+++ src/eval.c
@@ -0,0 +1,8 @@
+#include "eval.h"
+
+int eval(struct bekobuild_t *bekobuild) {
+  int i;
+  for (i = 0; i < bekobuild->build->size; ++i) {
+    system(string_vector_at(bekobuild->build, i));
+  }
+}
---
+++ src/eval.h
@@ -0,0 +1,8 @@
+#ifndef EVAL_H
+#define EVAL_H
+
+#include "bekobuild.h"
+
+extern int eval(struct bekobuild_t *);
+
+#endif
