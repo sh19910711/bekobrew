d227ad96f4953121d07957d127a71351d95975b0
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-25 03:09:21 +0900

    wip: command

--- src/command.c
+++ src/command.c
@@ -1,15 +1,18 @@
 #include "command.h"
+#include <string.h>
 
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
 
-static struct command sub_commands[] = {
-  { "version", sub_command_version },
-  { "help", sub_command_help },
-  { "install", sub_command_install },
-  { "build", sub_command_build },
-};
+/*** public functions ***/
 
 struct command *command_find(const char *cmd_name) {
+  static struct command sub_commands[] = {
+    { "version", sub_command_version },
+    { "help", sub_command_help },
+    { "install", sub_command_install },
+    { "build", sub_command_build },
+  };
+
   int i;
   for (i = 0; i < ARRAY_SIZE(sub_commands); i++) {
     struct command *p = sub_commands + i;
@@ -17,5 +20,6 @@ struct command *command_find(const char *cmd_name) {
       return p;
     }
   }
+
   return NULL;
 }
--- src/command.h
+++ src/command.h
@@ -1,22 +1,19 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 
-#include <stdio.h>
-#include <string.h>
-
 #include "string_vector.h"
-#include "version.h"
 
 struct command {
   const char *cmd;
   int (*call)(struct string_vector_t *);
 };
 
+extern struct command *command_find(const char *cmd_name);
+
+// The below functions are implemented in the "src/sub_command/*.c"
 extern int sub_command_version(struct string_vector_t *);
 extern int sub_command_help(struct string_vector_t *);
 extern int sub_command_install(struct string_vector_t *);
 extern int sub_command_build(struct string_vector_t *);
-
-extern struct command *command_find(const char *cmd_name);
 
 #endif
