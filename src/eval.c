bea3ad495a96edf0063f48ef70ec83da911c5b85
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-25 15:16:01 +0900

    wip: eval

--- src/eval.c
+++ src/eval.c
@@ -3,7 +3,12 @@
 /*** public functions ***/
 
 int eval(struct bekobuild_t *bekobuild) {
+  char command[512];
   int i;
+
+  sprintf(command, "mkdir -p %s", bekobuild->name);
+  system(command);
+
   for (i = 0; i < bekobuild->build->size; ++i) {
     system(string_vector_at(bekobuild->build, i));
   }
