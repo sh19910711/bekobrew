8d851f48cdd1be7dab20c396c698932dacca4920
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-25 17:12:28 +0900

    wip: test_helper

--- test/test_helper.h
+++ test/test_helper.h
@@ -6,6 +6,22 @@
 #include <sstream>
 #include <unistd.h>
 
+static inline std::string test_tmpdir() {
+  char buf[256];
+
+  FILE *proc = popen("mktemp", "r");
+  fgets(buf, 256, proc);
+  pclose(proc);
+
+  return std::string(buf);
+}
+
+static inline void test_rmdir(const char *path) {
+  std::stringstream ss;
+  ss << "rm -r " << path;
+  system(ss.str().c_str());
+}
+
 static inline void test_capture() {
   testing::internal::CaptureStdout();
 }
