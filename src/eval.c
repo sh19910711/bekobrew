8450eff8ea65109c6e0c6594236e352822e4f916
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-26 18:48:49 +0900

    wip: eval

--- src/eval.c
+++ src/eval.c
@@ -1,22 +1,44 @@
 #include "eval.h"
 #include "request.h"
+#include "url.h"
 
+#include <stdio.h>
 #include <sys/stat.h>
 
 static int calc_script_length(struct string_vector_t *commands);
 static void append(char **, const char *);
-static void get_sources(struct string_vector_t *, struct string_vector_t *);
+static void get_sources(const char *, struct string_vector_t *, struct string_vector_t *);
 
 /*** public functions ***/
+
+static int is_directory(const char *path) {
+  struct stat st;
+  return !stat(path, &st) && S_ISDIR(st.st_mode);
+}
+
+static int mkdir_p(const char *path, const mode_t mode) {
+  if (is_directory(path)) {
+    return 0;
+  }
+  char *s = strdup(path);
+  char *p = s;
+  char *q = p;
+  while (q = strchr(p, '/')) {
+    *q = '\0';
+    mkdir(s, mode);
+    *q = '/';
+    p = q + 1;
+  }
+}
 
 int eval(struct bekobuild_t *bekobuild) {
   char command[512];
   char *script;
 
-  mkdir(bekobuild->srcdir, 0755);
-  mkdir(bekobuild->pkgdir, 0755);
+  mkdir_p(bekobuild->srcdir, 0755);
+  mkdir_p(bekobuild->pkgdir, 0755);
 
-  get_sources(bekobuild->sources, bekobuild->sums);
+  get_sources(bekobuild->srcdir, bekobuild->sources, bekobuild->sums);
 
   script = eval_get_script(bekobuild->build);
   system(script);
@@ -58,16 +80,35 @@ static int calc_script_length(struct string_vector_t *commands) {
   return len;
 }
 
-static void get_sources(struct string_vector_t *sources, struct string_vector_t *sums) {
+static void write_file(const char *srcdir, const char *name, const char *body) {
+  if (strchr(name, '/')) {
+    fputs("ERROR: the file name doesn't contain slash.", stderr);
+    return;
+  }
+
+  char path[256];
+  sprintf(path, "%s/%s", srcdir, name);
+  printf("path: %s\n", path);
+
+  FILE *fp = fopen(path, "w");
+  fwrite(body, sizeof(char), strlen(body), fp);
+  fclose(fp);
+}
+
+static void get_sources(const char *srcdir, struct string_vector_t *sources,
+                        struct string_vector_t *sums) {
   if (!sources) {
     return;
   }
 
   int i;
   for (i = 0; i < sources->size; ++i) {
-    const char *url = string_vector_at(sources, i);
-    fprintf(stdin, "URL: %s\n", url);
-    char *res = request(url);
+    const char *url_str = string_vector_at(sources, i);
+    char *res = request(url_str);
+    struct url_t *url = url_parse(url_str);
+    write_file(srcdir, url_get_filename(url), res);
+
+    url_free(url);
     free(res);
   }
 }
